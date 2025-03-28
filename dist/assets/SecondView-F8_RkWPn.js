2ac6c80075bf311ca66a4cb97e13ab5105864530
mxmxmm25 <mxmxggmm25@gmail.com>
2025-03-28 11:50:38 +0300

    remove dist

--- dist/assets/SecondView-F8_RkWPn.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as a,a as n,F as o,o as t}from"./index-DEYyzPIx.js";const _=e({__name:"SecondView",setup(r){return(c,s)=>(t(),a("main",null,[n(o,{id:2})]))}});export{_ as default};
--- dist/assets/ThirdView-CdT0JJAU.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as a,a as n,F as o,o as r}from"./index-DEYyzPIx.js";const m=e({__name:"ThirdView",setup(t){return(c,s)=>(r(),a("main",null,[n(o,{id:3})]))}});export{m as default};
